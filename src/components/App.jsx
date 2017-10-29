35d9c51d6c273d6d0ecddf1c002c575595b24616
batuhan sahan <batuhansahan@trakya.edu.tr>
2017-10-29 15:22:45 +0000

    Rendering a list of reminders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,23 @@ class App extends Component {
     
     addReminder(){
         this.props.addReminder(this.state.text);
-        
+    }
+    
+    renderReminders(){
+        const { reminders } = this.props;
+        return (
+            <ul className="list-group col-sm-4">
+                {
+                    reminders.map(reminder => {
+                        return (
+                            <li key={reminder.id} className="list-group-item">
+                                <div>{reminder.text}</div> 
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        )
     }
     
     render(){
@@ -21,13 +37,14 @@ class App extends Component {
                 <div className="title">
                     Reminder Pro
                 </div>
-                <div className="form-inline">
+                <div className="form-inline reminder-form">
                     <div className="form-group">
                         <input 
                             className="form-control"
                             placeholder="I have to ..."
                             onChange={event => this.setState({text: event.target.value})}
                         />
+                    </div>
                         <button 
                             type="button"
                             className="btn btn-success"
@@ -35,15 +52,17 @@ class App extends Component {
                         >
                             Reminder
                         </button>
-                    </div>
                 </div>
+                { this.renderReminders() }
             </div>
         )
     }
 }
 
 function mapStateToProps(state){
-    console.log('state', state);
+    return {
+        reminders: state
+    }
 }
 
 export default connect(mapStateToProps, { addReminder })(App);
